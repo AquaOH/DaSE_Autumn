1b57821f67bf77dbdea3804644b2fa0305a0aed1
AquaOH <2863896505@qq.com>
2023-09-19 16:05:58 +0800

    实践5

---
+++ 第二周课后实践/2.5.py
@@ -0,0 +1,11 @@
+import math
+
+c = float(input("请输入数字: "))
+h = float(input("请输入精度: "))
+g = c / 2
+cnt = 0
+while math.fabs(math.pow(g, 2) - c) > h:
+    g = (g + c / g) / 2
+    cnt += 1
+print(g)
+print(cnt)
